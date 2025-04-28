9f7fe6ca6f324eec12699462244c1897e9ae6cd9
xinglu <xinglu@126.com>
2025-04-28 23:31:49 +0800

    bce loss

---
+++ 03_Coding_Problems/01_Top_Hands_On_Coding/04_Loss_Functions/bce.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def binary_cross_entropy(y_true, y_pred_prob):
+    """
+    计算二元交叉熵损失
+    
+    y_true: 真值类别, 0或者1, (N,)
+    y_pred_prob: 预测为1的概率, (N,)
+    """
+    # 为了防止log(0)导致数值问题, 对y_pred_prob数组进行裁剪
+    epsilon = 1e-15
+    np.clip(y_pred_prob, epsilon, 1 - epsilon)
+    
+    loss = -(y_true * np.log(y_pred_prob) + (1 - y_true) * np.log(1 - y_pred_prob))
+    return np.mean(loss)
+
+
+if __name__ == "__main__":
+    y_true = np.array([1, 0, 1, 0])
+    y_pred_prob  = np.array([0.9, 0.2, 0.8, 0.1])
+    bce_loss = binary_cross_entropy(y_true, y_pred_prob)
+    print(bce_loss)
+    
